e46aedeed91509f6a4f848d15d37b3dbc99f7cbb
ashley williams <leynessa1@gmail.com>
2022-05-07 16:34:31 -0300

    user and candle models

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -52,6 +53,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
